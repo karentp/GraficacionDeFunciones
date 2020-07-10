3e2e3c44251970a96b441b22a94501d62fc428b9
Karen Tovar <tovarparrakaren@gmail.com>
2020-07-10 12:15:26 -0500

    Ingreso y validación de comandos con argparse

--- graficacion_de_funciones_en_python/graficacion_de_funciones_en_python/funciones.py
+++ graficacion_de_funciones_en_python/graficacion_de_funciones_en_python/funciones.py
@@ -1,5 +1,25 @@
-from sympy import *
+"""
+
+En este archivo se encuentran las funciones que utiliza 
+el programa principal
+"""
+
+import sys
 
 
-def ingreso_datos():
-    pass
+def leer_linea(path):
+    """
+
+    Función que lee el archivo de entrada y retorna la expresion matematica.
+
+    :param string: dirección del archivo
+    :return string: Expresión en la primera línea
+    """
+    try:
+        file = open(path, "r")
+        first_line = file.readline()
+        file.close()
+        return first_line
+    except IOError:
+        print("El archivo no existe o no se tienen los permisos adecuados")
+        sys.exit()
--- graficacion_de_funciones_en_python/graficacion_de_funciones_en_python/graficacion_de_funciones_en_python.py
+++ graficacion_de_funciones_en_python/graficacion_de_funciones_en_python/graficacion_de_funciones_en_python.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         '-i',
         type=float,
         required='-t' not in sys.argv
-      
+    
     )
 
     parser.add_argument(
@@ -32,7 +32,7 @@ if __name__ == '__main__':
         '-s',
         type=float,
         required='-t' not in sys.argv
-     
+    
     )
 
     parser.add_argument(
@@ -40,7 +40,7 @@ if __name__ == '__main__':
         '-p',
         type=int,
         required='-t' not in sys.argv
-    
+ 
     )
     parser.add_argument(
         '--cifrassignificativas',
@@ -52,8 +52,7 @@ if __name__ == '__main__':
         '--derivadas',
         '-d',
         type=int,
-        default=0, 
-    
+        default=0,   
     )
 
     parser.add_argument(
@@ -67,61 +66,93 @@ if __name__ == '__main__':
     if(args.terminalinteractiva is True):
 
         while(True):
-            yfuncion = parse_expr(input("Ingrese la función de y, a graficar: "))
+            try:
+                yfuncion = parse_expr(
+                    input("Ingrese la función de y, a graficar: "))
+            except SyntaxError:
+                print("La expresión matemática es invalida")
 
             try:
                 derivadas = int(input("Número de derivadas por graficar:\n" +
-                                  "0 : ninguna, 1: primera derivada, " +
-                                  "2: segunda derivada: "))
+                                      "0 : ninguna, 1: primera derivada, " +
+                                      "2: segunda derivada: "))
             except ValueError:
-                print("\nError: Las derivadas deben ser entero 0,1,2 o dejarlo vacio")
+                print("\nError: "
+                      + "Las derivadas deben ser entero 0,1,2 o dejarlo vacio")
                 continue
             try:
-                limite_inferior = float(input("¿Cuál es el límite inferior de x?: "))
-                limite_superior = float(input("¿Cuál es el límite superior de x?: "))
+                limite_inferior = float(
+                                  input("¿Cuál es el límite inferior de x?: "))
+                limite_superior = float(
+                                  input("¿Cuál es el límite superior de x?: "))
                 assert limite_inferior < limite_superior
             except AssertionError:
-                print("\n Error: El límite inferior debe ser menor al límite superior")
+                print("\n Error: "
+                      + "El límite inferior debe ser menor al límite superior")
                 continue
 
             try:
-                paso = float(input("Ingrese el paso (diferencia entre cada x): "))
+                paso = float(
+                       input("Ingrese el paso (diferencia entre cada x): "))
                 assert paso > 0 and paso < (limite_superior-limite_inferior)
             except ValueError:
                 print("\n Error: el paso debe ser flotante")
                 continue
             except AssertionError:
-                print("\n Error: el paso debe ser mayor a 0 y menor al rango de x")
+                print("\n Error: "
+                      + "el paso debe ser mayor a 0 y menor al rango de x")
                 continue
 
             try:
-                cifras_significativas = int(input("¿Cuántas cifras" +
+                cifras_significativas = int(input("¿Cuántas cifras " +
                                                   "significativas desea: "))
                 assert cifras_significativas > 1
             except ValueError:
                 print("\n Error: las cifras significativas deben ser enteros")
                 continue
             except AssertionError:
-                print("\n Error: las cifras significativas deben ser mayor a 1")
+                print("\nError:las cifras significativas deben ser mayor a 1")
                 continue
 
             archivo_guardar = input("Ingrese el archivo donde se guardará" +
                                     "la gráfica (opcional): ")
             break
-            
-
-
+        
     else:
 
         archivo_entrada = args.archivoentrada
-        yfuncion = leerlinea(archivo_entrada)
+        yfuncion = leer_linea(archivo_entrada)
+        try:
+            yfuncion = parse_expr(yfuncion)
+        except SyntaxError:
+            print("La expresión matemática es invalida")
+            sys.exit()
+        
         derivadas = int(args.derivadas)
-        limite_inferior = float(args.limiteinferior)
-        limite_superior = float(args.limitesuperior)
-        paso = float(args.paso)
-        cifras_significativas = int(args.cifrassignificativas)
+        try:
+            limite_inferior = float(args.limiteinferior)
+            limite_superior = float(args.limitesuperior)
+            assert limite_inferior < limite_superior
+        except AssertionError:
+            print("\n Error: "
+                  + "El límite inferior debe ser menor al límite superior\n")
+            sys.exit()
+
+        try:
+            paso = float(args.paso)
+            assert paso > 0 and paso < (limite_superior-limite_inferior)
+
+        except AssertionError:
+            print("\n Error: el paso debe ser mayor a 0 y menor al rango de x")
+            sys.exit()
+     
+        try:
+            cifras_significativas = int(args.cifrassignificativas)
+            assert cifras_significativas > 1
+        except AssertionError:
+            print("\n Error: las cifras significativas deben ser mayor a 1")
+            sys.exit()
+               
         archivo_guardar = args.guardar
-    
-
-
-    
+ 
+    print("holi")
